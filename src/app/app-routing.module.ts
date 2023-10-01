d07486ce3bafd3f56d0e283c2c63b5745edc3bf0
natecodex <nathanielhaywood66@gmail.com>
2023-10-01 14:27:52 +0000

    i made changes to these pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './main-features/product-list/product-list.component';
+import { CheckoutComponent } from './main-features/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'checkout', component: CheckoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
